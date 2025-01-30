432dbe39d06b0a902eae93ff0a10a2e38410edc3
Lakori4 <santivalde04@gmail.com>
2025-01-30 13:49:36 +0100

    Initial commit

---
+++ main_graphql.ts
@@ -0,0 +1,34 @@
+import { MongoClient } from "mongodb"
+import { ApolloServer } from "@apollo/server"
+import { startStandaloneServer} from "@apollo/server/standalone"
+
+import { typeDefs } from "./schema.ts"
+import { resolvers } from "./resolvers.ts"
+import { ContactModel } from "./types.ts";
+
+const mUrl = Deno.env.get("mongo_url")
+
+if (!mUrl) {
+  throw new Error("No mongo url provided")
+}
+
+const client = new MongoClient(mUrl);
+
+await client.connect();
+
+const mongoDB = client.db("");
+
+const contactCollection = mongoDB.collection<>("");
+
+
+const server = new ApolloServer ({
+    typeDefs,
+    resolvers,
+})
+
+const { url } = await startStandaloneServer(server, {
+    context: async () => ({contactCollection})
+})
+
+
+console.log(`🚀  Server ready at ${url}`);
